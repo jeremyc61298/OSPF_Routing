ac8b0e98d2ffb21904d3b68f312a427343bd3c3a
Jeremy Campbell <jeremyc61298@gmail.com>
2018-10-26 13:07:35 -0500

    Added Dijkstra's algorithm.

--- OSPF_Routing/ospf.cpp
+++ OSPF_Routing/ospf.cpp
@@ -7,48 +7,96 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <set>
 
 using std::cerr;
 using std::endl;
 using std::ifstream;
 using std::ofstream;
 using std::vector;
+using std::set;
 
 class OSPF_Area
 {
 public:
 	explicit OSPF_Area(int numRouters);
 	void inputAreaMap(ifstream &fin);
-	void calculateShortestPaths();
-	void printShortestPaths(ofstream &fout, vector<int> startingRouters);
+	void calculateShortestPaths(int startRouter);
+	void printShortestPaths(ofstream &fout, int startRouter);
 
 private:
-	vector<vector<int>> adjacency;
+	// Data (Speeds are measured in Bps)
+	vector<vector<unsigned long long>> adjacency; 
+	vector<int> parents;
+	vector<unsigned long long> speeds;
+	set<int> routers;
 
-	void printShortestPaths(ofstream &fout, int startRouter);
+	// Methods
+	void printAdjacency();
 };
 
 OSPF_Area::OSPF_Area(int numRouters)
 {
-	adjacency.resize(numRouters, vector<int>(numRouters, 0));
+	adjacency.resize(numRouters, vector<unsigned long long>(numRouters, 0));
+	parents.resize(numRouters, -1);
+	speeds.resize(numRouters, ULLONG_MAX);
+
+	// Place routers in the set
+	for (int i = 0; i < numRouters; i++)
+		routers.insert(i);
 }
 
 void OSPF_Area::inputAreaMap(ifstream &fin)
 {
+	int numRouters = adjacency.size();
+	for (int i = 0; i < numRouters; i++)
+	{
+		for (int j = 0; j < numRouters; j++)
+		{
+			unsigned long long Bps;
+			fin >> Bps;
+			adjacency[i][j] = Bps;
+		}
+	}
 
+	//printAdjacency();
 }
 
 // Dijkstra's Shortest Path Algorithm
-void OSPF_Area::calculateShortestPaths()
+void OSPF_Area::calculateShortestPaths(int startRouter)
 {
+	set<int> t;
+	speeds[startRouter] = 0;
 
-}
-
-void OSPF_Area::printShortestPaths(ofstream &fout, vector<int> startRouters)
-{
-	for (int router : startRouters)
+	while (t != routers)
 	{
-		printShortestPaths(fout, router);
+		int selected; 
+		unsigned long long smallestValue = ULLONG_MAX;
+
+		for (int router : routers)
+		{
+			// for all the routers not in t that are in "routers"
+			if (t.find(router) == t.end() && speeds[router] < smallestValue)
+			{
+				selected = router;
+				smallestValue = speeds[router];
+			}
+		}
+
+		t.insert(selected);
+
+		for (int router : routers)
+		{
+			// for all routers not in t that are adjacent to selected
+			if (t.find(router) == t.end() && adjacency[selected][router] != 0)
+			{
+				if (speeds[selected] + adjacency[selected][router] < speeds[router])
+				{
+					speeds[router] = speeds[selected] + adjacency[selected][router];
+					parents[router] = selected;
+				}
+			}
+		}
 	}
 }
 
@@ -60,6 +108,22 @@ void OSPF_Area::printShortestPaths(ofstream &fout, int startRouter)
 		fout << " ";
 	else
 		fout << endl;
+}
+
+// Debug Method
+void OSPF_Area::printAdjacency()
+{
+	cerr << "Adjacency: " << endl;
+	cerr << "----------------------------------------" << endl;
+	for (vector<unsigned long long> row : adjacency)
+	{
+		for (unsigned long long Bps : row)
+		{
+			cerr << Bps << " ";
+		}
+		cerr << endl;
+	}
+	cerr << "----------------------------------------" << endl;
 }
 
 int main()
@@ -79,18 +143,15 @@ int main()
 	while (numRouters != 0)
 	{
 		ospfArea.inputAreaMap(fin);
-		ospfArea.calculateShortestPaths();
 
-		// Read in the routers wanted to start from
-		vector<int> startRouters;
 		int startRouter;
 		fin >> startRouter;
 		while (startRouter != 0)
 		{
-			startRouters.push_back(startRouter);
+			ospfArea.calculateShortestPaths(startRouter);
+			ospfArea.printShortestPaths(fout, startRouter);
+			fin >> startRouter;
 		}
-
-		ospfArea.printShortestPaths(fout, startRouters);
 		
 		fin >> numRouters;
 		if (numRouters != 0)
