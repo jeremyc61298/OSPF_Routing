5a31dfd18bc878ddae0c1c0125a49660f16256cf
Jeremy Campbell <jeremyc61298@gmail.com>
2018-10-26 11:57:07 -0500

    "main" now has the program flow. OSPF_Area class skeleton created.

--- OSPF_Routing/ospf.cpp
+++ OSPF_Routing/ospf.cpp
@@ -8,11 +8,59 @@
 #include <fstream>
 #include <vector>
 
-
 using std::cerr;
 using std::endl;
 using std::ifstream;
 using std::ofstream;
+using std::vector;
+
+class OSPF_Area
+{
+public:
+	explicit OSPF_Area(int numRouters);
+	void inputAreaMap(ifstream &fin);
+	void calculateShortestPaths();
+	void printShortestPaths(ofstream &fout, vector<int> startingRouters);
+
+private:
+	vector<vector<int>> adjacency;
+
+	void printShortestPaths(ofstream &fout, int startRouter);
+};
+
+OSPF_Area::OSPF_Area(int numRouters)
+{
+	adjacency.resize(numRouters, vector<int>(numRouters, 0));
+}
+
+void OSPF_Area::inputAreaMap(ifstream &fin)
+{
+
+}
+
+// Dijkstra's Shortest Path Algorithm
+void OSPF_Area::calculateShortestPaths()
+{
+
+}
+
+void OSPF_Area::printShortestPaths(ofstream &fout, vector<int> startRouters)
+{
+	for (int router : startRouters)
+	{
+		printShortestPaths(fout, router);
+	}
+}
+
+void OSPF_Area::printShortestPaths(ofstream &fout, int startRouter)
+{
+	//TODO: Print all the paths from the startRouter
+
+	if (startRouter != 0)
+		fout << " ";
+	else
+		fout << endl;
+}
 
 int main()
 {
@@ -24,5 +72,28 @@ int main()
 	}
 
 	ofstream fout("ospf.out");
+	int numRouters;
+	fin >> numRouters;
+	OSPF_Area ospfArea(numRouters);
 
+	while (numRouters != 0)
+	{
+		ospfArea.inputAreaMap(fin);
+		ospfArea.calculateShortestPaths();
+
+		// Read in the routers wanted to start from
+		vector<int> startRouters;
+		int startRouter;
+		fin >> startRouter;
+		while (startRouter != 0)
+		{
+			startRouters.push_back(startRouter);
+		}
+
+		ospfArea.printShortestPaths(fout, startRouters);
+		
+		fin >> numRouters;
+		if (numRouters != 0)
+			fout << "*****" << endl;
+	}
 }
